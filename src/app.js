c1aa781bfc5234c55d281b9460f408c3c49c7b56
Nairobi Bodden <breaniebodden@yahoo.com>
2022-10-24 21:01:05 -0600

    Proyecto inicializado

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.set("port", 4000);
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(app.get("port"), () => {
+  console.log("Servidor inicado en el puerto " + app.get("port"));
+});
