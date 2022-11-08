b45ebdf9d3f8cd702932bc4c2c14ecb02836abc8
Emerson Zapata <emerson.zapatarivas@gmail.com>
2022-11-07 22:56:39 -0600

    :)

--- src/rutas/salidas.js
+++ src/rutas/salidas.js
@@ -1,8 +1,18 @@
 const{Router}= require('express');
 const controladorSalidas = require('../controladores/salidas');
 const ruta = Router();
+const {body, query}= require('express-validator');
 ruta.get('/listar',controladorSalidas.Listar);
-ruta.post('/guardar',controladorSalidas.Guardar);
-ruta.put('/editar',controladorSalidas.Editar);
+ruta.post('/guardar',
+body("idCliente").isInt().withMessage("Solo se aceptan valores enteros para el idCliente"),
+body("fecha_Salida").isDate().withMessage("Solo se aceptan valores enteros para el numLote"),
+body("SucursalId").isInt().withMessage("Solo se aceptan valores enteros para el idSucursal"),
+controladorSalidas.Guardar);
+ruta.put('/editar',
+query("id").isInt().withMessage("Solo se aceptan valores enteros para el id de entrada"),
+body("idCliente").isInt().withMessage("Solo se aceptan valores enteros para el idCliente"),
+body("fecha_Salida").isDate().withMessage("Solo se aceptan valores de fechas para el fecha_Salida"),
+body("idSucursal").isInt().withMessage("Solo se aceptan valores enteros para el idSucursal"),
+controladorSalidas.Editar);
 ruta.delete('/eliminar',controladorSalidas.Eliminar);
 module.exports = ruta;
